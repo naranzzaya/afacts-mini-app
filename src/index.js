3450a53a8efdb403f15363a51b0a344bf2711ae1
Zay <z.znaranzaya@gmail.com>
2026-02-18 10:56:12 +0300

    user form

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css'
 
 function initSubscriptionFrom() {
-  const form = document.querySelector('form')
+  const form = document.getElementById('subscription_form')
   const input = document.querySelector('input[type=email]')
   const submit = document.querySelector('input[type=submit]')
   const url = form.action
@@ -47,6 +47,65 @@ function initSubscriptionFrom() {
   })
 }
 
+function initLoginForm() {
+  const form = document.getElementById('login_form')
+  const url = form.action
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(form)
+
+    fetch(url, {
+      method: 'POST',
+      body: formData
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+  })
+
+  submit.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const params = {
+      subscriber: {
+        email: input.value
+      }
+    }
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(params),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data)
+
+        const container = document.createElement('div')
+
+        const message = document.createElement('p')
+        message.innerText = data.success_text
+        message.style.marginTop = '20px'
+        message.style.fontSize = '2rem'
+
+        const link = document.createElement('a')
+        link.innerText = 'Посмотрите последние Q&A'
+        link.href = '/preview.html'
+
+        container.appendChild(message)
+        container.appendChild(link)
+        form.replaceWith(container)
+      })
+      .catch((error) => {
+        console.error('Ошибка при запросе:', error)
+      })
+  })
+}
 function initPreviewPage() {
   const container = document.querySelector('.posts')
   const url = container.dataset.url
@@ -59,6 +118,7 @@ function initPreviewPage() {
 document.addEventListener('DOMContentLoaded', () => {
   if (document.body.classList.contains('index')) {
     initSubscriptionFrom()
+    initLoginForm()
   } else if (document.body.classList.contains('preview')) {
     initPreviewPage()
   }
